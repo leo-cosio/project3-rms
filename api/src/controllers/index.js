4c4d623159eafa9b99b475c582026f4d6657da32
leo-cosio <lcosio2004@gmail.com>
2026-08-05 17:36:58 +0200

    fix: fixed router export

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -2,3 +2,5 @@ const { Router } = require("express");
 const { auth } = require("../middlewares/auth.mid");
 const config = require("../lib/config");
 const router = Router();
+
+module.exports = router;
