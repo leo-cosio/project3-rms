1bca4ae179bafd3f4277c58e12ed5ad9c0bf5bd2
leo-cosio <lcosio2004@gmail.com>
2026-08-10 17:47:40 +0200

    chore: table model

--- api/src/lib/models/table.model.js
+++ api/src/lib/models/table.model.js
@@ -1,4 +1,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const tableSchema = new Schema({});
+const tableSchema = new Schema({
+  number: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  location: {
+    type: String,
+    required: true,
+    enum: ["terraza", "sala", "otra"],
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["libre", "ocupada", "pendiente"],
+    required: true,
+  },
+  capacity: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 12,
+  },
+});
+
+const Table = mongoose.model("Table", tableSchema);
+module.exports = Table;
